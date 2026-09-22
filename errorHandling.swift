1a270f1eb70ac2c0eb8754c8fbd88357d866c5f3
EduardaMFerreira <>
2026-09-22 17:56:33 -0300

    add doc for errorHandling

--- errorHandling.swift
+++ errorHandling.swift
@@ -1,9 +1,20 @@
 // Tratamento de erros em Swift
+/* 
+ Este arquivo demonstra como tratar erros em Swift usando enumerações que conformam o protocolo Error,
+ a função que lança erros com 'throws', e o bloco 'do-catch' para capturar e lidar com esses erros.
+
+ Para realizar as construção, precisamos de 3 etapas:
+ 1. Definir os erros possíveis usando uma enumeração que conforma o protocolo Error.
+    - Informar o Swift sobre os possíveis erros que podem ocorrer.
+ 2. Criar uma função que lança erros usando 'throws'.
+    - Escrever uma função que possa sinalizar erros caso eles ocorram.
+ 3. Utilizar o bloco 'do-catch' para capturar e lidar com os erros.
+    - Chamar essa função e tratar quaisquer erros que possam ocorrer.
+*/
 
 enum PasswordError: Error {
     case short, obvious
 }
-
 
 func checkPassword(_ password: String) throws -> String {
     if password.count < 5 { throw PasswordError.short }
@@ -26,9 +37,9 @@ do {
     // Mas se algum erro for gerado, todo bloco do 'do' termina e ele pula direto para o bloco 'catch' passando para o print: Ocorreu um erro;
     print("Senha retorna: \(result)")
 } catch PasswordError.short {
-    print("Crie uma senha com mais de 5 caracteres!")
+    print("Senha muito curta!")
 } catch PasswordError.obvious {
-    print("Crie uma senha que não tenha números seguidos!")
+    print("Senha muito óbvia!")
 } catch {
     print("Ocorreu um erro!") //mensagem generiaca configurada da apple
 }
